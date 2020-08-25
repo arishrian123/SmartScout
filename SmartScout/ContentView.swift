61c156fdda407decbcfa54065da04f41cb7e795b
arishrian123 <arishrian123@gmail.com>
2020-08-25 15:58:43 +0100

    Initial Commit

---
+++ SmartScout/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SmartScout
+//
+//  Created by Arish Tripathi on 25/08/2020.
+//  Copyright © 2020 Arish Tripathi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
