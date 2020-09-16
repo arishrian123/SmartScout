734e0f0708a28b251e5c9584750637ec9832d467
Olliecord <68302704+Olliecord@users.noreply.github.com>
2020-09-16 02:59:59 +0100

    Login design

--- SmartScout/LoginView.swift
+++ SmartScout/LoginView.swift
@@ -7,6 +7,7 @@
 //
 
 import SwiftUI
+import SDWebImageSwiftUI
 import FirebaseAuth
 import FirebaseFirestore
 
@@ -16,9 +17,13 @@ struct LoginView: View {
     @State var password = ""
     @State var isSuccess = false
     @State var userType = 0
+    @State var moveAlongCircle = false
     var userTypes = ["Parent", "Scout"]
     let db = Firestore.firestore()
     var data: VideoStore
+    
+    let width = UIScreen.main.bounds.width
+    let height = UIScreen.main.bounds.height
     
     func login() {
         
@@ -30,9 +35,9 @@ struct LoginView: View {
             } else if let result = result {
                 
                 /*self.db.collection("users")
-                    .document(result.user.uid)
-                    .setData(["email": self.email,
-                              "type": self.userTypes[self.userType]])*/
+                 .document(result.user.uid)
+                 .setData(["email": self.email,
+                 "type": self.userTypes[self.userType]])*/
                 
                 self.isSuccess = true
             }
@@ -42,55 +47,114 @@ struct LoginView: View {
     var body: some View {
         
         ZStack{
-        if !isSuccess {
-        ZStack{
-            Color.init("color1").edgesIgnoringSafeArea(.all)
-            
-            HStack {
-                VStack{
+            if !isSuccess {
+                ZStack {
+                    
+                    Circle()
+                        .frame( width: width/3, height: height/3)
+                        .foregroundColor(Color("color1"))
+                        .offset(x: width/1.9, y: height/(-2.8))
+                        .shadow(radius: 10)
+                    
+                    Circle()
+                        .frame( width: width/8.5, height: height/8.5)
+                        .foregroundColor(Color("color1"))
+                        .offset(x: width/4, y: height/(-2.8))
+                        .shadow(radius: 10)
+                    
+                    Circle()
+                        .frame( width: width/3, height: height/3)
+                        .foregroundColor(Color("color1"))
+                        .offset(x: -width/1.9, y: height/2.45)
+                        .shadow(radius: 10)
+                                      
+                    
+                    Circle()
+                        .frame( width: width/8.5, height: height/8.5)
+                        .foregroundColor(Color("color1"))
+                        .offset(x: -width/4, y: height/2.45)
+                        .shadow(radius: 10)
+                                      
+                    
+                    Text("Sign Up")
+                        .foregroundColor(Color.black)
+                        .font(Font.custom("Poppins-SemiBold", size: 35))
+                        .offset(x: width/4, y: height/2.45)
+                    
                     Text("Login")
-                        .font(.system(size: 40, weight: .bold, design: .default))
-                        .foregroundColor(Color.white)
+                        .font(Font.custom("Poppins-Semibold", size: 40))
+                        .offset(x: -130, y: -320)
                     
-                    VStack{
+                    VStack {
                         
-                        TextField("Email", text: $email)
+                        HStack{
+                            Text("Smart")
+                                .foregroundColor(Color.black)
+                                .font(Font.custom("Poppins-Light", size: 25))
+                                .offset(x: 5)
+                            
+                            Text("Scout")
+                                .foregroundColor(Color.black)
+                                .font(Font.custom("Poppins-Medium", size: 25))
+                                .offset(x: -5)
+                        }
+                        VStack {
                         
+                            VStack(alignment: .leading) {
                         
-                    SecureField("Password", text: $password)
+                                HStack {
+                                    
+                                    TextField("  Email", text: $email)
+                                        .font(Font.custom("Poppins-Light", size: 20))
+                                        .frame(width: width/1.12, height: height/18)
+                                    
+                                    
+                                }.background(BlurView(style: .light))
+                                    
+                                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                                
+                                HStack {
+                                    SecureField("  Password", text: $password)
+                                        .font(Font.custom("Poppins-Light", size: 20))
+                                        .frame(width: width/1.12, height: height/18)
+                                    
+                                    
+                                }.background(BlurView(style: .light))
+                                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                                
+                                HStack {
+                                    
+                                    Spacer()
+                                    
+                                    Button(action: {}){
+                                        Text("Login")
+                                        .font(Font.custom("Poppins-Light", size: 20))
+                                        .foregroundColor((Color("color1")))
+                                    }
+                                    .padding()
+                                    .background(BlurView(style: .regular))
+                                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                                }.padding(.trailing, 20)
+                            }.padding()
+                        }
+                        .frame(width: width/1.05, height: height/4)
+                        .background(BlurView(style: .regular))
+                        .clipShape(RoundedRectangle(cornerRadius: 20))
+                    
+                        .padding(.top, height/3.5)
+                              Spacer()
                         
-                        
-                        /*Picker(selection: $userType, label: Text("Select")){
-                            ForEach(0 ..< userTypes.count){
-                                Text(self.userTypes[$0])
-                            }
-                        }*/
                     }
-                    
-                    Button(action: {
-                        self.login()
-                    }){
-                        Text("Click")
-                        .font(.system(size: 40, weight: .bold, design: .default))
-                        .foregroundColor(Color.white)
-                    }
-                    
-                    Spacer()
                 }
                 
                 
-                
-            }.padding()
-            
-            
-        }
-        } else {
-            TabBarView(data: data)
-        }
+            }
+            else {
+                TabBarView(data: data)
+            }
         }
     }
 }
-
 struct LoginView_Previews: PreviewProvider {
     static var previews: some View {
         LoginView(data: VideoStore())
